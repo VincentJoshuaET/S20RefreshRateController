d0d6dae8d8b4bf5e3965e126a6d5cadf2191e348
Vincent Joshua Tigas <vincentjoshuaet@nobel-systems.com>
2020-12-27 14:34:28 +0800

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vjet.s20refreshratecontroller"
         minSdkVersion(21)
         targetSdkVersion(21)
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
     }
 
     buildTypes {
